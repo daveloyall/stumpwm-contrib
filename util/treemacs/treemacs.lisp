db47f18136c927a49050bc54a5a9ead7d2b87703
Dave Loyall <dave@the-good-guys.net>
2015-12-10 14:47:17 -0600

    actually draw something; but it doesn't land in stump's window stack.

--- util/treemacs/treemacs.lisp
+++ util/treemacs/treemacs.lisp
@@ -1,13 +1,9 @@
 ;;;; treemacs.lisp
 
-(in-package #:treemacs
-	    (:use #:stumpwm))
+(in-package #:treemacs)
 
 ;;; "treemacs" goes here. Hacks and glory await!
 
-(defun draw-art ()
-  "draw some art. Provided by the treemacs module"
-  )  
 ;;(current-screen)
 ;;(screen-message-cc screen)
 
@@ -32,17 +28,63 @@
 ;;                                     :event-mask '(:exposure)))
 
 
+;; (defun make-mode-line-window (parent screen)
+;;   "Create a window suitable for a modeline."
+;;   (xlib:create-window
+;;    :parent parent
+;;    :x 0 :y 0 :width 1 :height 1
+;;    :background (alloc-color screen *mode-line-background-color*)
+;;    :border (alloc-color screen *mode-line-border-color*)
+;;    :border-width *mode-line-border-width*
+;;    ;; You can click the modeline
+;;    :event-mask (xlib:make-event-mask :button-press :exposure)
+;;    ;; these windows are not controlled by the window manager
+;;    :override-redirect :on))
 
-(defun setup-art-window (screen width height)
-  (let ((win (screen-message-window screen)))
-    ;; Now that we know the dimensions, raise and resize it.
-    (xlib:with-state (win)
-      (setf (xlib:drawable-height win) height
-            (xlib:drawable-width win) (+ width (* *message-window-padding* 2))
-            (xlib:window-priority win) :above)
-      (setup-win-gravity screen win *message-window-gravity*))
-    (xlib:map-window win)
-    (incf (screen-ignore-msg-expose screen))
-    ;; Have to flush this or the window might get cleared
-    ;; after we've already started drawing it.
-    (xlib:display-finish-output *display*)))
+
+(defun setup-art-window (screen win width height)
+   
+  ;; raise and resize it.
+  (xlib:with-state (win)
+    (setf (xlib:drawable-height win) height
+	  (xlib:drawable-width win) (+ width (* *message-window-padding* 2))
+	  (xlib:window-priority win) :above)
+    (stumpwm::setup-win-gravity screen win *message-window-gravity*))
+  (xlib:map-window win)
+  ;; Have to flush this or the window might get cleared
+  ;; after we've already started drawing it.
+  (xlib:display-finish-output *display*))
+
+
+
+
+(defun make-art-window-gc (window screen)
+  (xlib:create-gcontext :drawable window
+                        :font (when (typep (stumpwm::screen-font screen) 'xlib:font) (stumpwm::screen-font screen))
+                        :foreground (stumpwm::alloc-color screen *mode-line-foreground-color*)
+                        :background (stumpwm::alloc-color screen *mode-line-background-color*)))
+
+
+(defun make-art-window ()
+  "\"It is good to love many things, for therein lies the true strength,
+     and whosoever loves much performs much, and can accomplish much,
+     and what is done in love is well done.\" --Vincent van Gogh"
+  (let* ((screen (current-screen))
+	 (screen-number (stumpwm::screen-number screen)))
+    (let ((window (xlib:create-window :parent (xlib:screen-root screen-number)
+			:x 100 :y 0 :width 200 :height 100
+			:background (stumpwm::alloc-color screen "black")
+			:bit-gravity :north-east
+			:border (stumpwm::alloc-color screen "gold")
+			:border-width 3
+			:colormap (xlib:screen-default-colormap
+				   screen-number)
+			:event-mask '(:exposure))))
+      (make-art-window-gc window screen)
+      (setup-art-window screen window 50 300))
+))
+
+(defcommand draw-art () ()
+  "draw some art. Provided by the treemacs module"
+  (make-art-window))  
+
